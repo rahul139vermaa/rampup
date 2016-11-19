f7fa245cc1e15621c87069ff897510c71fd18001
rahul <rahul.verma02@snapdeal.com>
2016-11-19 18:06:58 +0530

    added comments

--- src/main/scala/batchrampup/D2WithDF.scala
+++ src/main/scala/batchrampup/D2WithDF.scala
@@ -31,14 +31,38 @@ object D2WithDF {
     val modDF = personsDF.select(personsDF("age"),personsDF("name"),-personsDF("age")+currentYear as "YOB")
 
     //2.2
+    /*
+    Obj2.2 Find average age of Person using 'age' class member.
+    Jobs - 1
+    Stages -2
+    Tasks - 3
+    Partitions - 2
+    */
+
     val resAvg = modDF.select(avg(modDF("age")))
     resAvg.show()
 
     //2.3
+    /*
+    Obj2.3 Find sum of  age of Person using 'age' class member.
+    Jobs - 1
+    Stages - 2
+    Tasks - 3
+    Partitions - 2
+     */
+
     val resSum = modDF.select(sum(modDF("age")) )
     resSum.show()
 
     //2.4
+    /*
+    Obj2.4 Group people with their name's initial character, and calculate the average age for each initial.
+    Jobs - 2
+    Stages - 3
+    Tasks - 202
+    Partitions - 199
+     */
+
     val avgAgeGroupByInitial= modDF.groupBy(modDF("name").substr(0,1)).avg("age")
     avgAgeGroupByInitial.show()
 
--- src/main/scala/batchrampup/D2WithRDD.scala
+++ src/main/scala/batchrampup/D2WithRDD.scala
@@ -10,14 +10,14 @@ object D2WithRDD {
 
   import batchrampup.D2._
 
-  def Ob1={
+  def Ob1 = {
     //done previously
     /*Ob1. Create an object - List[Person] with 1 million objects
       (in case the shell crashes, have lesser objects) with random name and ages(range 1-100).
       Save this list to HDFS as json/parquet/csv file format.*/
   }
 
-  def Ob2={
+  def Ob2 = {
 
     /*Ob2. Use following methods to explore different operations on this List object to:
        Obj2.1 In the list add the year-of-birth to get new list - List[Person, Int].
@@ -26,28 +26,51 @@ object D2WithRDD {
        Obj2.4 Group people with their name's initial character, and calculate the average age for each initial.  */
 
 
-    val persons=sc.parallelize(
-      PersonUtils.fromJson(sc.textFile(Constants.HDFS_URI+Constants.FILE_PATH).first)
+    val persons = sc.parallelize(
+      PersonUtils.fromJson(sc.textFile(Constants.HDFS_URI + Constants.FILE_PATH).first)
     )
 
     //2.1
-    val currentYear =Calendar.getInstance().get(Calendar.YEAR)
+    val currentYear = Calendar.getInstance().get(Calendar.YEAR)
     val modList = persons.map(p => (p, currentYear - p.age))
     //println(modList.collect().toList)
 
     //2.3
+    /*
+    Obj2.3 Find sum of  age of Person using 'age' class member.
+
+    Jobs - 1
+    Stages - 1
+    Tasks - 2
+    Partitions - 2
+    */
+
     val sum = modList.map(_._1.age).sum
     //OR (seems same as sum will use fold method)
-    val sumUsingReduce =modList.map(_._1.age).reduce((a,b) => a+b)
+    val sumUsingReduce = modList.map(_._1.age).reduce((a, b) => a + b)
     println(sum)
     println(sumUsingReduce)
 
     //2.2
+    /*Obj2.2 Find average age of Person using 'age' class member.
+
+      Jobs - 1
+      Stages -1
+      Tasks - 2
+      Partitions - 2   */
+
     val avg = sum / modList.count
     println(avg)
 
     //2.4
-    val avgAgeGroupByInitial= {
+    /*
+    Obj2.4 Group people with their name's initial character, and calculate the average age for each initial.
+    Jobs - 1
+    Stages - 2
+    Tasks - 4
+    Partitions - 2
+    */
+    val avgAgeGroupByInitial = {
       val pairRdd = modList.map(x => (x._1.name.head, x._1.age))
       val gbk = pairRdd.groupByKey
       gbk.map(x => (x._1, x._2.sum / x._2.size)).collect
